cd81600acd286b484689aa1f8fb3cd14a5518931
Yangyu Chen <cyy@cyyself.name>
2022-04-09 17:20:07 +0800

    all tests

--- gen_tests.py
+++ gen_tests.py
@@ -6,12 +6,16 @@ file_list = []
 # build riscv-tests to this folder
 BUILD_DIR = "../riscv-tests/build/isa"
 DST_DIR = "./tests"
+TEST_PRIFIX = ["rv64ui-v-","rv64um-v-","rv64ua-v-","rv64mi-p-","rv64si-p-"]
 for (dirpath, dirnames, filenames) in os.walk(BUILD_DIR):
     for x in filenames:
         if x.endswith(".dump"):
-            pass
-        elif x.startswith("rv64ui-v-"):
-            file_list.append(x)
+            continue
+        for y in TEST_PRIFIX:
+            if x.startswith(y):
+                file_list.append(x)
+file_list.sort()
+
 def make_test():
     os.system("mkdir -p {}".format(DST_DIR))
     for x in file_list:
