37847ed2c8c29655753cd40150908f0d1c76c47d
Yangyu Chen <cyy@cyyself.name>
2024-06-25 14:41:08 +0800

    mmio_dev: add virtual deconstruct function

--- src/memory/mmio_dev.hpp
+++ src/memory/mmio_dev.hpp
@@ -7,6 +7,7 @@ class mmio_dev {
 public:
     virtual bool do_read (uint64_t start_addr, uint64_t size, char* buffer) = 0;
     virtual bool do_write(uint64_t start_addr, uint64_t size, const char* buffer) = 0;
+    virtual ~mmio_dev() {}
 };
 
 #endif
